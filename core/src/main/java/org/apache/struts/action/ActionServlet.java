cf663c42c51db32d2ca752a0c9575b1256aca8f8
root <jsurf@gmx.de>
2017-06-16 10:56:39 +0200

    Apply fedora patch -p2 < struts-1.3.10-CVE-2016-1181-CVE-2016-1182.patch

--- core/src/main/java/org/apache/struts/action/ActionServlet.java
+++ core/src/main/java/org/apache/struts/action/ActionServlet.java
@@ -23,6 +23,7 @@ package org.apache.struts.action;
 import org.apache.commons.beanutils.BeanUtils;
 import org.apache.commons.beanutils.ConvertUtils;
 import org.apache.commons.beanutils.PropertyUtils;
+import org.apache.commons.beanutils.SuppressPropertiesBeanIntrospector;
 import org.apache.commons.beanutils.converters.BigDecimalConverter;
 import org.apache.commons.beanutils.converters.BigIntegerConverter;
 import org.apache.commons.beanutils.converters.BooleanConverter;
@@ -76,6 +77,7 @@ import java.net.URLConnection;
 
 import java.util.ArrayList;
 import java.util.Enumeration;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.MissingResourceException;
@@ -1700,8 +1702,19 @@ public class ActionServlet extends HttpServlet {
      */
     protected void initOther()
         throws ServletException {
-        String value;
 
+        /* Fix CVE-2016-1181 CVE-2016-1182 start */
+        HashSet suppressProperties = new HashSet();
+        suppressProperties.add("class");
+        suppressProperties.add("multipartRequestHandler");
+        suppressProperties.add("resultValueMap");
+
+        PropertyUtils.addBeanIntrospector(
+                new SuppressPropertiesBeanIntrospector(suppressProperties));
+        PropertyUtils.clearDescriptors();
+
+        String value = null;
+        /* Fix CVE-2016-1181 CVE-2016-1182 end */
         value = getServletConfig().getInitParameter("config");
 
         if (value != null) {
